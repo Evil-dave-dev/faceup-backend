b4a4f96cc7049d5e8432f6c1f0ff9c59f0556857
David <evil.dave59@gmail.com>
2023-12-07 13:03:55 +0100

    add cloudinary config

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,23 @@ const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 const uniqid = require("uniqid");
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 router.post("/upload", async (req, res) => {
   // const photoPath = `./tmp/${uniqid()}.jpg`;
   // const resultMove = await req.files.photoFromFront.mv(photoPath);
-  res.json({ result: true, file: req.files.photoFromFront });
+  // res.json({ result: true, file: req.files.photoFromFront });
 
   // if (!resultMove) {
-  // const resultCloudinary = await cloudinary.uploader.upload(
-  //   req.files.photoFromFront
-  // );
+  const resultCloudinary = await cloudinary.uploader.upload(
+    req.files.photoFromFront
+  );
   // // fs.unlinkSync(photoPath);
-  // res.json({ result: true, url: resultCloudinary.secure_url });
+  res.json({ result: true, url: resultCloudinary.secure_url });
   // } else {
   //   res.json({ result: false, error: resultMove });
   // }
